ef7f407b78300f58bc278730a9dc368ac4a67105
sgtpeter30 <sgtpeter30@gmail.com>
2023-10-08 13:46:48 +0200

    first steps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,11 +13,12 @@ import { PresentsComponent } from './tree/presents/presents.component';
 import { PresentComponent } from './tree/presents/present/present.component';
 import { LetterComponent } from './tree/letters/letter/letter.component';
 import { BaseUrlInterceptor } from './http-interceptor';
+import { WriteLetterComponent } from './tree/letters/write-letter/write-letter.component';
 
 export const environment = {
   production: true,
-  apiUrl: "https://straszna-fabryka-swietego-mikolaja.loca.lt"
-  // apiUrl: "http://localhost:3000"
+  // apiUrl: "https://straszna-fabryka-swietego-mikolaja.loca.lt"
+  apiUrl: "http://localhost:3000"
 };
 
 @NgModule({
@@ -29,6 +30,7 @@ export const environment = {
     PresentsComponent,
     PresentComponent,
     LetterComponent,
+    WriteLetterComponent,
   ],
   imports: [
     BrowserModule,
@@ -39,6 +41,7 @@ export const environment = {
       { path: '', component: TreeComponent},
       { path: 'tree/letters', component: LettersComponent},
       { path: 'tree/letters/letter/:id', component: LetterComponent},
+      { path: 'tree/letters/write-letter', component: WriteLetterComponent},
       { path: 'tree/presents', component: PresentsComponent},
       { path: 'tree/presents/present/:id', component: PresentComponent},
       { path: 'admin', component: HomeComponent},
